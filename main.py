a3fdc9380408637995df18c2e72c599a80a16ba2
Einars Skutans <einarsskutans@gmail.com>
2023-01-30 00:59:19 +0200

    keomafe

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ request = requests.get(url)
 
 page = request.content
 html = BeautifulSoup(page, 'lxml')
+
+articles = html.find_all('a', class_='text-mine-shaft')
+
+class Article():
+    title = ''
+    content = ''
+    link = ''
+
+for i in range(50):
+    
